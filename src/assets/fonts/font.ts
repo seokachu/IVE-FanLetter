b1a65b8424b5cc39487ea09c3798adf8269e984e
박서영 <seokachuu@gmail.com>
2024-09-06 00:00:47 +0900

    [Feat] 기본폰트 추가, reset.css 추가

---
+++ src/assets/fonts/font.ts
@@ -0,0 +1,11 @@
+import { Noto_Sans_KR, Hi_Melody } from "next/font/google";
+
+export const notoSansKr = Noto_Sans_KR({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const hiMelody = Hi_Melody({
+  weight: ["400"],
+  subsets: ["latin"],
+});
---
+++ src/utils/TextTyping.tsx
@@ -0,0 +1,5 @@
+const TextTyping = () => {
+  return <div></div>;
+};
+
+export default TextTyping;
