4900a1d0b4134aa229ed9c486c85a5fb03627a9b
박서영 <seokachuu@gmail.com>
2024-09-10 05:13:35 +0900

    [Feat] 조건에 따라 header image logo변경 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,22 +6,20 @@ import MainLogoImage from "@/assets/images/logo.svg";
 import SubLogoImage from "@/assets/images/logo_black.svg";
 import S from "@/styles/common.module.scss";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const pathname = usePathname();
   const isMainPage = pathname === "/";
   const headerStyle = !isMainPage ? S.active : "";
+  const logoSrc = headerStyle ? SubLogoImage : MainLogoImage;
 
   return (
     <div className={`${S.headerWrapper} ${headerStyle}`}>
       <header className={S.header}>
         <h1>
           <Link href="/">
-            <Image
-              src={!headerStyle ? MainLogoImage : SubLogoImage}
-              alt="logo"
-              priority
-            />
+            <Image src={logoSrc} alt="logo" priority />
           </Link>
         </h1>
         <Nav />
